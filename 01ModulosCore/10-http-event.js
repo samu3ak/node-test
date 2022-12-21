b3254f787d161a3461b7dfebd55d2e13a4bcff7a
Samuel Bolívar <samuelbolivar2003@gmail.com>
2022-12-21 15:51:16 +0100

    node http module practice fs

---
+++ 01ModulosCore/10-http-event.js
@@ -0,0 +1,10 @@
+var http = require('http').createServer()
+function webServer(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1>Hola Node.js</h1><br/><b>Buenos dias Samuel Bolivar</b>')
+}
+http
+    .on('request', webServer)
+    .listen(3000, 'localhost')
+
+console.log('Servidor corriendo en http://localhost:3000/')
