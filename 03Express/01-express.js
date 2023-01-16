a2a20af31ad0ffe65d5c4869a29391cda2617b9b
Samuel Bolívar <samuelbolivar2003@gmail.com>
2023-01-16 18:35:25 +0100

    mongodb CRUD

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 require("dotenv").config();
 const port = process.env.PORT || 3005; // Hacemos uso de las variables de entorno
